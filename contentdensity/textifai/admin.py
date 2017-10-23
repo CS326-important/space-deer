566752151173ebb305725ab66ceeee6f7191bfe0
Ozias Gonet <oziasgonet@gmail.com>
2017-10-23 14:32:33 -0400

    Basic admin page functionality for Text and Insight. Also added some mock data.

--- contentdensity/textifai/admin.py
+++ contentdensity/textifai/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Text, Insight
 
 # Register your models here.
+
+@admin.register(Text)
+class TextAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Insight)
+class InsightAdmin(admin.ModelAdmin):
+    pass
