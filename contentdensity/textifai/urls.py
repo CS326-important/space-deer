392b07d32274839e5d050a4ea147070edf615caf
Bynine <byninegiga@gmail.com>
2017-10-26 13:51:51 -0400

    working account page

--- contentdensity/textifai/urls.py
+++ contentdensity/textifai/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^textinput', views.textinput, name='textinput'),
     url(r'^featureoutput', views.featureoutput, name='featureoutput'),
+    url(r'^account', views.account, name='account'),
 ]
 
 
