edc478d2fa87b189a66ab45627da729e3677efc6
Brandon Yannoni <byannoni+github@gmail.com>
2017-10-29 23:35:38 -0400

    Add general insights calculator
    Also create a modules folder for other modules to be added to as
    necessary.

---
+++ contentdensity/textifai/modules/gic.py
@@ -0,0 +1,44 @@
+
+from functools import reduce
+from ..models import User, Text, Insight, Comment, GeneralInsight
+
+class general_insight_calculator:
+    name = None
+    calc = lambda *_: None
+
+    def __init__(self, name, calc):
+        self.name = name
+        self.calc = calc
+
+    def do_calc(self):
+        return self.calc()
+
+    def calc_and_save(self):
+        entry, created = GeneralInsight.objects.get_or_create(pk=self.name, defaults={'value':''})
+        entry.value = self.do_calc()
+        entry.save()
+
+general_insights = [ ]
+
+def add_general_insight(name, func):
+    global general_insights
+    general_insights.append(general_insight_calculator(name, func))
+
+def calc_and_save_general_insights():
+    for insight in general_insights:
+        insight.calc_and_save()
+
+########################################################################
+# Insight calculation implementations
+########################################################################
+
+def _calc_total_words():
+    ret = 0
+
+    for text in Text.objects.all():
+        ret += len(text.content.split())
+
+    return ret
+
+add_general_insight('Total Words', _calc_total_words)
+
