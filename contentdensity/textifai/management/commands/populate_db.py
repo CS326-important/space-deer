c525335b370c916175bf9bb3d0635ac6051e6d9b
Bynine <byninegiga@gmail.com>
2017-10-30 16:12:52 -0400

    fixed db population script

--- contentdensity/textifai/management/commands/populate_db.py
+++ contentdensity/textifai/management/commands/populate_db.py
@@ -1,13 +1,13 @@
 from django.core.management.base import BaseCommand
-from textifai.models import User
+from textifai.models import User, Text, Insight, Comment, GeneralInsight
 
 class Command(BaseCommand):
     args = '<>'
     help = 'Run this script to populate database w/ sample users.'
 
     def create_users(self):
-        user = User(username='tom',,email='tomthebomb@hotmail.com')
-        user.save()
+        user1 = User(username='tom',email='tomthebomb@hotmail.com')
+        user1.save()
 
     def handle(self, *args, **options):
         self.create_users()
