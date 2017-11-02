baf4731472073982067f91b23babc9da7b80fbb0
Ash Johns <ashleypjohns@gmail.com>
2017-11-02 01:04:37 -0400

    Fix populate_db runtime errors
    Fix the populate_db runtime errors by removing and m_id and
    time_created assignments (those are done automatically).
    Add more data

--- contentdensity/textifai/management/commands/populate_db.py
+++ contentdensity/textifai/management/commands/populate_db.py
@@ -14,20 +14,21 @@ class Command(BaseCommand):
     text3 = None
 
     def create_users(self):
-        self.user1 = User(m_id=uuid.uuid4(), username='tom', email='tomthebomb@hotmail.com')
+        self.user1 = User(username='tom', email='tomthebomb@hotmail.com')
         self.user1.save()
-        self.user2 = User(m_id=uuid.uuid4(), username='michelle', email='shipluvr@aol.net')
+        self.user2 = User(username='michelle', email='shipluvr@aol.net')
         self.user2.save()
         self.user3 = User(username='William Shakespeare', email='billshakes@gmail.com')
         self.user3.save()
+        self.user4 = User(username='Steve', email='coffee4steve@gmail.com')
+        self.user4.save()
 
     def create_texts(self):
-        self.text1 = Text(m_id=uuid.uuid4(), content='Wherefore art thou, Romeo?', time_created=datetime.now(), user=self.user1, mature_content = False)
+        self.text1 = Text(content='Wherefore art thou, Romeo?', user=self.user1, mature_content = False)
         self.text1.save()
-        self.text2 = Text(m_id=uuid.uuid4(), content='I\'m going to attack you with a ship', time_created=datetime.now(), user=self.user2, mature_content = True)
+        self.text2 = Text(title='Salty Sailor', content='I\'m going to attack you with a ship', user=self.user2, mature_content = True)
         self.text2.save()
-        self.text3 = Text(m_id=uuid.uuid4()
-                          , content="""
+        self.text3 = Text(content="""
             Ham. O, I die, Horatio; The potent poison quite oer-crows my spirit:
             I cannot live to hear the news from England; But I do prophesy the election
             lights On Fortinbras: he has my dying voice; So tell him, with the occurrents,
@@ -35,16 +36,26 @@ class Command(BaseCommand):
             Now cracks a noble heart. Good night, sweet prince, And flights of angels
             sing thee to thy rest! Why does the drum come hither? [March within.]"
             """
-                          , time_created=datetime.now()
                           , user=self.user1
                           , mature_content = False
         )
         self.text3.save()
+        self.text4 = Text(title="Espresso", content="""
+        Extraction, iced siphon aroma to go, that filter percolator a beans. Steamed
+        carajillo con panna est con panna arabica est filter extra. Grounds ristretto
+        macchiato id sweet, spoon cinnamon black carajillo aromatic. Siphon caffeine,
+        brewed blue mountain americano blue mountain extra blue mountain. In, percolator
+        froth est ut pumpkin spice seasonal so redeye breve. Milk cappuccino cup, cinnamon
+        wings cinnamon milk sweet. Brewed café au lait, java, caffeine, ristretto, siphon
+        doppio body organic eu that. Et, barista whipped kopi-luwak percolator single origin
+        skinny rich fair trade. Coffee galão sugar cappuccino sit carajillo cup milk kopi-luwak aroma.
+        """, user = self.user4, mature_content = False)
+        self.text4.save()
 
     def create_insights(self):
-        insight1 = Insight(m_id=uuid.uuid4(), tone='Sad', probability=0.93, text=self.text1, user=self.user1)
+        insight1 = Insight(tone='Sad', probability=0.93, text=self.text1, user=self.user1)
         insight1.save()
-        insight2 = Insight(m_id=uuid.uuid4(), tone='Violent', probability=0.83, text=self.text2, user=self.user2)
+        insight2 = Insight(tone='Violent', probability=0.83, text=self.text2, user=self.user2)
         insight2.save()
         insights = [('Negative', 0.69), ('Shakespeare', 0.92), ('Death', 0.99),
                     ('Politics', 0.84), ('Music', 0.51)]
@@ -56,12 +67,12 @@ class Command(BaseCommand):
         Takes a list of (tone, probability) tuples.
         """
         for t in insights:
-            Insight(m_id=uuid.uuid4(), tone=t[0], probability=t[1], text=text, user=user).save()
+            Insight(tone=t[0], probability=t[1], text=text, user=user).save()
 
     def create_comments(self):
-        comment1 = Comment(m_id=uuid.uuid4(), content='I like this text.', text=self.text2, time_created=datetime.now(), user=self.user1)
+        comment1 = Comment(content='I like this text.', text=self.text2, user=self.user1)
         comment1.save()
-        comment2 = Comment(m_id=uuid.uuid4(), content='You can reduce the violence of this text by replacing "attack" with something more positive, like "bring you to Hawaii".', text=self.text1, time_created=datetime.now(), user=self.user2)
+        comment2 = Comment(content='You can reduce the violence of this text by replacing "attack" with something more positive, like "bring you to Hawaii".', text=self.text1, user=self.user2)
         comment2.save()
         Comment(content="Cool! Did you write this? :^)", text=self.text3,
                 user=self.user2).save()
