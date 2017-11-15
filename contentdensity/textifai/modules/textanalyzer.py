86b01cfaae90557324716e28b0a4171b585534f2
Ozias Gonet <oziasgonet@gmail.com>
2017-11-15 00:22:01 -0500

    Readability score implemented.

--- contentdensity/textifai/modules/textanalyzer.py
+++ contentdensity/textifai/modules/textanalyzer.py
@@ -51,8 +51,49 @@ class TextAnalyzer:
         return Counter(pos_list).most_common(1)[0][0]
 
     def _get_reading_level(self):
-        # todo
-        return None
+        """
+        https://en.wikipedia.org/wiki/Dale%E2%80%93Chall_readability_formula
+        """
+
+        readability_score = self._get_dale_chall_readability_score()
+
+        if readability_score <= 4.9:
+            return "4th grade"
+        elif readability_score <= 5.9:
+            return "5-6th grade"
+        elif readability_score <= 6.9:
+            return "7-8th grade"
+        elif readability_score <= 7.9:
+            return "9-10th grade"
+        elif readability_score <= 8.9:
+            return "11-12th grade"
+        else:
+            return "college"
+
+    def _get_dale_chall_readability_score(self):
+        word_list = self.text.split()
+        return self._dale_chall_formula(
+            self._get_num_difficult_words(word_list),
+            self._get_num_sentences(),
+            len(word_list))
+
+    def _get_num_difficult_words(self, word_list):
+        dale_chall_list = self._get_dale_chall_list()
+        easy_words = set(reduce(lambda x, y: x + y, dale_chall_list).split())
+        return reduce(
+            lambda x, y: x + 1 if y not in easy_words else x, word_list, 0)
+
+    def _get_dale_chall_list(self):
+        with open('./textifai/modules/dale-chall-words.txt') as f:
+            return f.readlines()
+
+    def _get_num_sentences(self):
+        sentences = len(self.text.split('.')) - 1
+        return sentences if sentences >= 1 else 1
+
+    def _dale_chall_formula(self, difficult_words, sentences, words):
+        return 0.1579 * ((difficult_words / words) * 100) \
+               + 0.0496 * (words / sentences)
 
     def _format_time_string(self, seconds):
         if seconds < 60:
