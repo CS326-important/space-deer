0e1d12dd4804a1e00eadc0311e9bdd7061fc5a1a
Ozias Gonet <oziasgonet@gmail.com>
2017-12-10 19:06:20 -0500

    Heroku stuffs.

--- contentdensity/contentdensity/settings.py
+++ contentdensity/contentdensity/settings.py
@@ -83,6 +83,11 @@ DATABASES = {
     }
 }
 
+# Heroku database config  
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -123,3 +128,19 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/textifai/account'
+
+# Heroku related static assets config
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
